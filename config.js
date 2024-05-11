275f6f940bc9b50e7b200b6042af2574d7b3fa40
hunyKR <sdg1200g@gmail.com>
2024-05-11 14:52:17 +0900

    수정2

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
     MONGODB_CONNECTION_STRING: `mongodb+srv://app:1234@app.qf6fpfp.mongodb.net/`,
-    CORS_ORIGIN: 'https://miniboard-client.netlify.app/',
+    CORS_ORIGIN: 'https://miniboard-client.netlify.app',
     HOST: '0.0.0.0',
     PORT: '9000'
 }
